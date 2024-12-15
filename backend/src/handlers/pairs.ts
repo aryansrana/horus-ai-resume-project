4af5304a625b7ab30e1d8212738279c0e08a9529
PillarSpam <beet@DESKTOP-L6E7EMF>
2024-12-15 16:13:27 -0500

    changed fit score calculation

--- backend/src/handlers/pairs.ts
+++ backend/src/handlers/pairs.ts
@@ -54,6 +54,53 @@ class AnalysisHandler{
             return;
         }
     }
+    static async comparison(req: Request, res: Response){
+        try{
+            const {resume_id, description_id} = req.body
+            if(!resume_id){
+                res.status(400).json({ error: 'Resume Id not given.', status: 'error' })
+            }
+            if(!description_id){
+                res.status(400).json({ error: 'Job Description Id not given.', status: 'error' })
+            }
+            try{
+                const resume_text = await ResumeService.extract_text_from_resume(resume_id);
+                if(!resume_text){
+                    res.status(400).json({ error: 'Resume text is empty.', status: 'error' })
+                    return;
+                }
+                if(resume_text.length > 10000){
+                    res.status(400).json({ error: 'Resume text exceeds the maximum of 10000 characters.', status: 'error' })
+                    return;
+                }
+                const description = await Description.findById(description_id);
+                if(!description){
+                    res.status(400).json({ error: 'Description not found.', status: 'error' })
+                    return;
+                }
+                const description_text = description.job_description;
+                if(!description_text){
+                    res.status(400).json({ error: 'Job Description text is empty.', status: 'error' })
+                    return;
+                }
+                if(description_text.length > 10000){
+                    res.status(400).json({ error: 'Resume text exceeds the maximum of 10000 characters.', status: 'error' })
+                    return;
+                }
+                const result = await AnalysisService.compare(resume_text, description_text);
+                res.status(200).json({fit_score: result.fit_score});
+            }
+            catch(error){
+                res.status(500).json({ error: (error as Error).message, status: 'error' });
+                return;
+            }
+        }
+        catch(error){
+            console.error(error);
+            res.status(500).json({ error: (error as Error).message, status: 'error' });
+            return;
+        }
+    }
 }
 
 export default AnalysisHandler;
